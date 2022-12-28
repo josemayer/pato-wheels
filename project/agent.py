04757e8bd5cdcda15c93053ae3b98f4975bb6d52
José Lucas Silva Mayer <josemayer@usp.br>
2022-12-28 07:04:29 -0400

    Atualização do código para remover o Braitenberg
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- project/agent.py
+++ project/agent.py
@@ -72,18 +72,6 @@ class Agent:
         self.inner_upper = np.array([45, 255, 255])
         self.outer_lower = np.array([0, 0, 130])
         self.outer_upper = np.array([179, 85, 255])
-        # Acquire image for initializing activation matrices
-        img = self.env.front()
-        img_shape = img.shape[0], img.shape[1]
-        self.inner_left_motor_matrix = np.zeros(shape=img_shape, dtype="float32")
-        self.inner_right_motor_matrix = np.zeros(shape=img_shape, dtype="float32")
-        self.outer_left_motor_matrix = np.zeros(shape=img_shape, dtype="float32")
-        self.outer_right_motor_matrix = np.zeros(shape=img_shape, dtype="float32")
-        # Connecition matrices
-        self.inner_left_motor_matrix[img_shape[0]//2:, :img_shape[1]//2] = -1.5
-        self.inner_right_motor_matrix[img_shape[0]//2:, img_shape[1]//2:] = -1.0
-        self.outer_left_motor_matrix[img_shape[0]//2:, :img_shape[1]//2] = -7/11
-        self.outer_right_motor_matrix[img_shape[0]//2:, img_shape[1]//2:] = -7/11
 
     def preprocess(self, image):
         """ Returns a 2D array mask color segmentation of the image """
@@ -97,7 +85,7 @@ class Agent:
         # produces combined mask (might or might not be useful)
         mask = cv2.bitwise_or(inner_mask, outer_mask)
         self.masked = cv2.bitwise_and(image, image, mask=mask)
-        return inner_mask, outer_mask, mask
+        return inner_mask, outer_mask
 
     def get_pwm_control(self, v: float, w: float)-> (float, float):
         ''' Takes velocity v and angle w and returns left and right power to motors.'''
@@ -110,81 +98,38 @@ class Agent:
         # acquire front camera image
         img = self.env.front()
 
+        # run image processing routines
+        P, Q = self.preprocess(img) # returns inner and outter mask matrices
+ 
         # transform image to shape (60, 80, 3)
         img_inference = cv2.resize(img, (80, 60))
-
-        # run image processing routines
-        P, Q, M = self.preprocess(img) # returns inner, outter and combined mask matrices
-        # build left and right motor signals from connection matrices and masks (this is a suggestion, feel free to modify it)
-        inner_left, inner_right = self.inner_left_motor_matrix, self.inner_right_motor_matrix
-        outer_left, outer_right = self.outer_left_motor_matrix, self.outer_right_motor_matrix
-        
+       
         # predict single image img_inference
         img_inference = np.expand_dims(img_inference, axis=0)
         prediction = self.model_od.predict(img_inference, verbose=False)
         if prediction[0] > 0.5:
             # activate the dodge model
             prediction = self.model_dodge.predict(img_inference, verbose=False)
-            pwm_left, pwm_right = self.get_pwm_control(prediction[0][0]*0.8, prediction[0][1]*1.25)
+            pwm_left, pwm_right = self.get_pwm_control(prediction[0][0], prediction[0][1])
             self.env.step(pwm_left, pwm_right)
             return
 
-        L = float(np.sum(P * inner_left)) + float(np.sum(Q * outer_left))
-        R = float(np.sum(P * inner_right)) + float(np.sum(Q * outer_right))
-
-        # Upper bound on the values above (very loose bound)
-        limit = img.shape[0]*img.shape[1]*2
-        # These are big numbers, better to rescale them to the unit interval
-        L = rescale(L, 0, limit)
-        R = rescale(R, 0, limit)
-        # Tweak with the constants below to get to change velocity or to stabilize the behavior
-        # Recall that the pwm signal sets the wheel torque, and is capped to be in [-1,1]
-        
-        # Normal gain and const, ideally while in straight line
-        gain = 12 #increasing this will increasing responsitivity and reduce stability
-        const = 0.5 # power under null activation - this affects the base velocity
-        
-        # If in a curve, increase gain to increase responsitivity
-        if abs(L-R) > 0.01:
-            gain = 20
-            const = 0.2
-
-        pwm_left = const + R * gain
-        pwm_right = const + L * gain
-        # print('>', L, R, pwm_left, pwm_right) # uncomment for debugging
-        # Now send command to motors
-       
-
         # resize masks P and Q to (60, 80)
-        p_inf = cv2.resize(P, (80, 60))
-        q_inf = cv2.resize(Q, (80, 60))
+        p_resized = cv2.resize(P, (80, 60))
+        q_resized = cv2.resize(Q, (80, 60))
 
         # create a 2-channel image with the masks
         mask = np.zeros((60, 80, 2))
-        mask[:, :, 0] = p_inf
-        mask[:, :, 1] = q_inf
+        mask[:, :, 0] = p_resized
+        mask[:, :, 1] = q_resized
         
         # cut off the 30% top pixels
         mask = mask[(3 * mask.shape[0])//10:, :, :]
 
-        # expand dims
         masks = np.expand_dims(mask, axis=0)
         prediction = self.model_lf.predict(masks, verbose=False)
-        pwm_left, pwm_right = self.get_pwm_control(prediction[0][0]*1.25, prediction[0][1])
+        pwm_left, pwm_right = self.get_pwm_control(prediction[0][0], prediction[0][1])
         self.env.step(pwm_left, pwm_right)
-        return
 
-        # If the difference between sums of two masks is too small (i.e. the agent is in a straight line) then increase the base velocity
-        if abs(L-R) < 0.002:
-            scale = -np.log(abs(L-R))
-            scale = min(scale, 1000)
-            pwm_left = scale * 0.5
-            pwm_right = scale * 0.5
-
-        self.env.step(pwm_left, pwm_right)
-        #  for visualization
+       #  for visualization
         self.env.render('human')
-
-def rescale(a: float, L: float, U: float):
-    ''' Map scalar x in interval [L, U] to interval [0, 1]. '''
-    return (a - L) / (U - L)
